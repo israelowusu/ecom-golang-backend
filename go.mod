7fb4ab5eec7458d62a8a9e9cc06f6a7c24ae6961
israelowusu <oaisrael633@gmail.com>
2025-01-30 13:46:35 +0000

    implement database migrations

--- go.mod
+++ go.mod
@@ -13,7 +13,12 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.24.0 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.18.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
--- types/types.go
+++ types/types.go
@@ -17,6 +17,32 @@ type User struct {
 	CreatedAt time.Time `json: "createdAt"`
 }
 
+type Product struct {
+	ID          int       `json: "id"`
+	Name        string    `json: "firstName"`
+	Description string    `json: "lastName"`
+	Image       string    `json: "email"`
+	Quantity    string    `json: "-"`
+	CreatedAt   time.Time `json: "createdAt"`
+}
+
+type Order struct {
+	ID        int       `json: "id"`
+	UserID    string    `json: "firstName"`
+	Total     string    `json: "lastName"`
+	Status    string    `json: "email"`
+	Address   string    `json: "-"`
+	CreatedAt time.Time `json: "createdAt"`
+}
+
+type Orders_Item struct {
+	ID        int    `json: "id"`
+	OrderID   string `json: "firstName"`
+	ProductID string `json: "lastName"`
+	Quantity  string `json: "email"`
+	Price     string `json: "-"`
+}
+
 type RegisterUserPayload struct {
 	FirstName string `json: "firstName" validate: "required"`
 	LastName  string `json: "lastName" validate: "required"`
